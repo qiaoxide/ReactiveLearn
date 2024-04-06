8a4e48a279acb93e180fbc599bc59c92e6a16eff
qiaoxide <qiaoxide@126.com>
2024-04-06 15:40:55 +0800

    更新

--- app/src/main/java/reactivelearn/ReactorDemo.java
+++ app/src/main/java/reactivelearn/ReactorDemo.java
@@ -2,12 +2,18 @@ package reactivelearn;
 
 import reactor.core.publisher.Mono;
 
+/***
+ * Reactor 实现异步非阻塞编程
+ */
 public class ReactorDemo {
 
     private static final long SLEEP_TIME = 1000;
 
     public static void main(String[] args) {
 
+        // 创建两个 Mono 对象，分别表示烧水和清洗工作。
+
+        // Mono.fromSupplier 用于创建一个 Mono 对象。Mono代表一个任务，它会在某个时刻产生一个结果。
         Mono<Boolean> boilWater = Mono.fromSupplier(() -> {
             try {
                 System.out.println("洗好水壶");
@@ -38,6 +44,7 @@ public class ReactorDemo {
             return true;
         });
 
+        // 非阻塞的方式同时运行两个任务异步任务，Mono.zip 是 Reactor 库中的一个方法，用于组合两个或更多的 Mono。
         Mono.zip(boilWater, washCup)
                 .subscribe(tuple -> {
                     if (tuple.getT1() && tuple.getT2())
